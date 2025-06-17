61d5fe2cc2a8afbec4e1ebfa76624841ba8a4fc2
d00telemental <4rterius@gmail.com>
2025-06-18 02:47:10 +0300

    add even more tests for tmap

--- Tests/Tests.TMap.hpp
+++ Tests/Tests.TMap.hpp
@@ -35,10 +35,78 @@ TEST_SUITE("TMap") {
     TEST_CASE("string to pointer") {
         TMap<FString, void*> Map{};
 
-        FString FirstKey{};
-        void* FirstValue = (void*)1;
+        {
+            FString FirstKey{};
+            void* FirstValue = (void*)1;
 
-        Map.Set(FirstKey, FirstValue);
-        CHECK_EQ(*Map.Find(std::move(FirstKey)), FirstValue);
+            Map.Set(FirstKey, FirstValue);
+            CHECK_EQ(*Map.Find(std::move(FirstKey)), FirstValue);
+        }
+
+        {
+            FString SecondKey{ L"cheers" };
+            void* SecondValue = (void*)2;
+
+            Map.Set(SecondKey, SecondValue);
+            CHECK_EQ(*Map.Find(std::move(SecondKey)), SecondValue);
+        }
+
+        {
+            FString ThirdKey{ L"s.kSDjdbgfYTdhf" };
+            void* ThirdValue = (void*)3;
+
+            Map.Set(ThirdKey, ThirdValue);
+            CHECK_EQ(*Map.Find(std::move(ThirdKey)), ThirdValue);
+        }
+
+        {
+            FString FourthKey{ L"91123286" };
+            void* FourthValue = (void*)4;
+
+            Map.Set(FourthKey, FourthValue);
+            CHECK_EQ(*Map.Find(std::move(FourthKey)), FourthValue);
+        }
+
+        {
+            FString FifthKey{ L";SDJFOSADHF" };
+            void* FifthValue = (void*)5;
+
+            Map.Set(FifthKey, FifthValue);
+            CHECK_EQ(*Map.Find(std::move(FifthKey)), FifthValue);
+        }
+
+        {
+            FString SixthKey{ L"sadllamas" };
+            void* SixthValue = (void*)6;
+
+            Map.Set(SixthKey, SixthValue);
+            CHECK_EQ(*Map.Find(std::move(SixthKey)), SixthValue);
+        }
+    }
+
+    TEST_CASE("string to string")
+    {
+        // kudos for amazing test strings, mgamerz
+
+        TMap<FString, FString> Map{};
+
+        for (int i = 1; i < 12; i++)
+        {
+            auto KeyValSet = FString::Printf(L"Poop%d", i);
+
+            Map.Set(KeyValSet, KeyValSet);
+            CHECK_NE(Map.Find(FString::Printf(L"Poop%d", i)), nullptr);
+            CHECK_EQ(*Map.Find(FString::Printf(L"Poop%d", i)), FString::Printf(L"Poop%d", i));
+        }
+
+        for (int i = 8; i < 12; i++)
+        {
+            auto KeyValSet = FString::Printf(L"Poop%d", i);
+
+            Map.Set(KeyValSet, KeyValSet);
+            CHECK_NE(Map.Find(FString::Printf(L"Poop%d", i)), nullptr);
+            CHECK_EQ(*Map.Find(FString::Printf(L"Poop%d", i)), FString::Printf(L"Poop%d", i));
+        }
+
     }
 }
